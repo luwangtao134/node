b34dd53a146316e30239d3e2192df5555dbf32e1
jack <843090608@qq.com>
2019-12-27 15:08:42 +0800

    ssss

---
+++ src/components/demo/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      这是demo组件
+      
+      {{name}}---{{age}}传值
+  </div>
+</template>
+<script type='text/javascript'>
+export default {
+  //传值
+  props:["name","age"],
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {}
+};
+</script>
+<style type='text/css' scoped>
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
-
       我是首页{{ name }} -- {{ age }}
+
+      <demo1 name="zbj" :age="age"></demo1>传值
   </div>
 </template>
 
 <script>
+//导入componets里的demo组件
+import Demo1 from '@/components/demo/index.vue'
 export default {
     data(){
         return {
             name:'zbj',
             age:18
         }
+    },
+    //导入componets里的demo组件
+    components:{
+        Demo1
     }
 }
 </script>
